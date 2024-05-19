09983791acf90cb0eb9ff686d78611864a4a10d5
Taneristique <48108872+Taneristique@users.noreply.github.com>
2024-05-19 17:02:49 +0300

    Update Form1.cs
    Puan hesaplama mantığı düzeltildi.

--- Form1.cs
+++ Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,7 +16,7 @@ namespace NotHesaplayıcı
     {
         ListBox list;
         bool geçerli_not;
-        private const string lisans= "MIT License\r\n\r\nCopyright (c) 2024 Taneristique\r\n\r\nPermission is hereby granted, free of charge, to any person obtaining a copy\r\nof this software and associated documentation files (the \"Software\"), to deal\r\nin the Software without restriction, including without limitation the rights\r\nto use, copy, modify, merge, publish, distribute, sublicense, and/or sell\r\ncopies of the Software, and to permit persons to whom the Software is\r\nfurnished to do so, subject to the following conditions:\r\n\r\nThe above copyright notice and this permission notice shall be included in all\r\ncopies or substantial portions of the Software.\r\n\r\nTHE SOFTWARE IS PROVIDED \"AS IS\", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR\r\nIMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,\r\nFITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE\r\nAUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER\r\nLIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,\r\nOUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE\r\nSOFTWARE.";
+        private const string lisans = "MIT License\r\n\r\nCopyright (c) 2024 Taneristique\r\n\r\nPermission is hereby granted, free of charge, to any person obtaining a copy\r\nof this software and associated documentation files (the \"Software\"), to deal\r\nin the Software without restriction, including without limitation the rights\r\nto use, copy, modify, merge, publish, distribute, sublicense, and/or sell\r\ncopies of the Software, and to permit persons to whom the Software is\r\nfurnished to do so, subject to the following conditions:\r\n\r\nThe above copyright notice and this permission notice shall be included in all\r\ncopies or substantial portions of the Software.\r\n\r\nTHE SOFTWARE IS PROVIDED \"AS IS\", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR\r\nIMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,\r\nFITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE\r\nAUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER\r\nLIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,\r\nOUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE\r\nSOFTWARE.";
 
         public Form1()
         {
@@ -27,7 +28,8 @@ namespace NotHesaplayıcı
         {
             float vize, final, result;
             geçerli_not = (float.Parse(textBox1.Text) >= 0 && float.Parse(textBox1.Text) <= 100) && (float.Parse(textBox2.Text) >= 0 && float.Parse(textBox2.Text) <= 100);
-            if (geçerli_not && textBox3.Text != "") {
+            if (geçerli_not && textBox3.Text != "")
+            {
                 try
                 {
                     vize = float.Parse(textBox1.Text);
@@ -53,10 +55,12 @@ namespace NotHesaplayıcı
             }
             else
             {
-                if (textBox3.Text == "" && geçerli_not==false) {
+                if (textBox3.Text == "" && geçerli_not == false)
+                {
                     MessageBox.Show("Formun ders adı bölümü boş bırakılamaz!\nAyrıca ders notları 100 dahil olmak üzere 0 ve 100 aralığında olmalıdır!");
                 }
-                else if(geçerli_not==false){
+                else if (geçerli_not == false)
+                {
                     MessageBox.Show("Ders notları 100 dahil olmak üzere 0 ve 100 aralığında olmalıdır!");
 
                 }
@@ -65,7 +69,7 @@ namespace NotHesaplayıcı
                     MessageBox.Show("Formun ders adı bölümü boş bırakılamaz");
                 }
             }
-            
+
         }
 
         private void button2_Click(object sender, EventArgs e)
@@ -90,19 +94,19 @@ namespace NotHesaplayıcı
 
         private void button3_Click(object sender, EventArgs e)
         {
-            int gereken_not;
-            int yeni_toplam;
-            int standart_hata;
+            float gereken_not;
+            float yeni_toplam;
+            float standart_hata;
             geçerli_not = int.Parse(textBox5.Text) >= 0 && int.Parse(textBox5.Text) <= 100;
             if (textBox4.Text != "" && textBox5.Text != "" && geçerli_not)
             {
                 try
                 {
-                    gereken_not = (100*(60- (30 * int.Parse(textBox5.Text)/100))) / 70; 
-                    yeni_toplam = ((int.Parse(textBox5.Text) * 30 / 100) + gereken_not * 70 / 100);
+                    gereken_not = (100 * (60 - (30 * int.Parse(textBox5.Text) / 100))) / 70;
+                    yeni_toplam = (int.Parse(textBox5.Text) * 30 / 100 )+ (gereken_not * 70 / 100);
                     standart_hata = 60 - yeni_toplam;
-                    gereken_not = standart_hata > 0 ? gereken_not + standart_hata : gereken_not;
-                    label10.Text = string.Format("{0} dersinden geçmek için final sınavından {1} almalısınız!", textBox4.Text, gereken_not.ToString());
+                    gereken_not = standart_hata > 0 || standart_hata < 0 ? gereken_not + standart_hata : gereken_not;
+                    label10.Text = string.Format("{0} dersinden geçmek için final sınavından {1} almalısınız!", textBox4.Text, Convert.ToInt32(gereken_not).ToString());
                 }
                 catch (Exception ex)
                 {
